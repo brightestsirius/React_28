62c289744e8a2f3f12dbeea2141ef06c4c052a5d
brightestsirius <katherina.shypovskaya@gmail.com>
2024-09-03 18:01:20 +0300

    lesson_06 – intro

---
+++ lesson_06/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Todos from "./components/Todos/Todos";
+
+export default function App() {
+  return (
+    <>
+      <Todos />
+    </>
+  );
+}
---
+++ lesson_06/src/components/Todos/Todos.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./style.sass";
+
+export default function Todos() {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const request = await fetch(
+          `https://6675570ea8d2b4d072efa0bb.mockapi.io/todos`
+        ),
+        response = await request.json();
+
+      setTodos(response);
+    } catch (err) {
+      console.log(`err`);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  });
+
+  const getClassName = (item) => {
+    const classes = [];
+    if (item.priority) classes.push(`item--priority`);
+    return classes.join(` `);
+  };
+
+  return todos.length ? (
+    <ul>
+      {todos.map((item) => (
+        <li className={getClassName(item)} key={item.id}>
+          {item.title}
+        </li>
+      ))}
+    </ul>
+  ) : null;
+}
